5566ecb3ce26352a1b16ffc5ba4945ede4f99f66
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2020-05-31 21:12:51 +0200

    Built web
    Branch: master
    Commit: 4edacebfd3d09e236238ed02956228d110e6730a
    
    package:peanut 3.4.2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "6e75cfb03250c61849878e0bf8933d76",
 "/": "6e75cfb03250c61849878e0bf8933d76",
-"main.dart.js": "acca8f584bb2ecf2246cbf3a143c1491",
+"main.dart.js": "40a89d665231a16b86f9c42bacfb42da",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
